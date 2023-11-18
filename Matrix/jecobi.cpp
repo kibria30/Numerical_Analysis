6e19249ac4096aceb3d82ca62b4a9fc7695a009a
kibria30 <bsse1430@iit.du.ac.bd>
2023-11-18 19:04:26 +0600

    first commit

---
+++ Matrix/jecobi.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    cout<<"Enter the number of unknowns of a linear system: ";
+    int n;
+    cin>>n;
+    double system[n][n+1];
+    cout<<"Enter the coefficient matrix: "<<endl;
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n+1;j++)
+        {
+            cin>>system[i][j];
+        }
+    }
+    //i have to check that this matrix is diagonally dominant or not
+    for(i=0;i<n;i++)
+    {
+        int sum=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                sum+=fabs(system[i][j]);
+            }
+
+        }
+        if(fabs(system[i][i])<sum)
+        {
+            cout<<"Not Diagonally Dominant"<<endl;
+            return 0;
+        }
+    }
+    double previous_solution[n];
+    double new_solution[n];
+    for(i=0;i<n;i++)
+    {
+        previous_solution[i]=0.0;
+    }
+    int flag=0;
+    int iteration=0;
+  // cout<<"iteration"<<" "<<"x\t"<<"y\t"<<"z"<<endl;
+    while(n)
+    {
+        iteration++;
+        for(i=0;i<n;i++)
+        {
+            new_solution[i]=system[i][n];
+            for(j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                    new_solution[i]-=previous_solution[j]*system[i][j];
+                }
+            }
+            new_solution[i]=new_solution[i]/system[i][i];
+        }
+         cout<<iteration<<" ";
+         for(i=0;i<n;i++)
+         {
+             cout<<new_solution[i]<<"\t";
+         }
+        cout<<endl;
+        for(i=0;i<n;i++)
+        {
+            if(fabs(previous_solution[i]-new_solution[i])>.00001)
+            {
+                flag=1;
+            }
+            else 
+            {
+                flag=0;
+            }
+        }
+        if(flag==0)
+        {
+            break;
+        }
+        else 
+        {
+            for(i=0;i<n;i++)
+            {
+                previous_solution[i]=new_solution[i];
+            }
+        }
+    }
+    cout<<"Iterration="<<iteration<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<new_solution[i]<<endl;
+    }
+   // cout<<iteration;
+}
